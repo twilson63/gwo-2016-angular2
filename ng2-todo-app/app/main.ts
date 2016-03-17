8d3e139515551088ebcca78ba88ce469eb9a52a6
Tom Wilson <tom@jackhq.com>
2016-03-16 23:30:27 -0400

    update example

--- ng2-todo-app/app/main.ts
+++ ng2-todo-app/app/main.ts
@@ -1,7 +1,7 @@
 import {bootstrap}    from 'angular2/platform/browser'
 import {AppComponent} from './app'
-
 import {TodoService} from './todo-service'
+import {HTTP_PROVIDERS} from 'angular2/http'
 
-bootstrap(AppComponent, [TodoService]);
+bootstrap(AppComponent, [TodoService, HTTP_PROVIDERS]);
     
